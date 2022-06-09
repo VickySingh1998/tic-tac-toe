e3633d3bb2523a27ce63a8197184d1375978ef2f
VickySingh1998 <vickysingh8697@gmail.com>
2022-06-10 02:46:32 +0530

    added: a tic tac toe board

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,7 +1,16 @@
 package com.bridgelabz;
 
 public class TicTacToeGame {
+    public static void showBoard() {
+        char[] BOARD = new char[10];
+
+        System.out.println(BOARD[0] + " | " + BOARD[1] + " | " + BOARD[2]);
+        System.out.println(BOARD[3] + " | " + BOARD[4] + " | " + BOARD[5]);
+        System.out.println(BOARD[6] + " | " + BOARD[7] + " | " + BOARD[8]);
+    }
     public static void main(String[] args) {
-        System.out.println("Welcome to Tic Tac Toe Game");
+        System.out.println("Welcome to Tic Tac Toe game");
+        showBoard();
+
     }
 }
