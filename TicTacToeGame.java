0bdbea59be0021d12d056d9e6e7b434784451381
VickySingh1998 <vickysingh8697@gmail.com>
2022-06-10 03:24:41 +0530

    added: player to input X or O

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,15 +1,27 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
 public class TicTacToeGame {
-    public static void showBoard() {
+
+    public static void showBard() {
         char[] BOARD = new char[10];
+        char player;
+        char computer;
 
         System.out.println(BOARD[0] + " | " + BOARD[1] + " | " + BOARD[2]);
         System.out.println(BOARD[3] + " | " + BOARD[4] + " | " + BOARD[5]);
         System.out.println(BOARD[6] + " | " + BOARD[7] + " | " + BOARD[8]);
     }
+
+    public static char player() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your choice x or 0 only");
+        return scanner.next().toUpperCase().charAt(0);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Welcome to Tic Tac Toe game");
-        showBoard();
+        System.out.println("Welcome to Tic Toc Toe game");
+        showBard();
+        player();
     }
 }
